49891e178de2b5acbccfe3e0766a59ac98ee44d0
Luiz Messias <luizgdsmdev@gmail.com>
2025-09-01 19:59:17 -0300

    Fix (app.js): adjust for routers.

--- aula-5/src/App.js
+++ aula-5/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/react-studies/aula-5/login" element={<Login />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
   );
 }
