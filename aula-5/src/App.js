2f2bdde4da86f2b1235aafc2f91cb0c9ec8f4e66
Luiz Messias <luizgdsmdev@gmail.com>
2025-09-01 19:50:01 -0300

    Update (form): adding react hook form + Zod for validation.

--- aula-5/src/App.js
+++ aula-5/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/react-studies/aula-5/login" element={<Login />} />
         </Routes>
   );
 }
--- aula-5/src/components/loginForm/index.js
+++ aula-5/src/components/loginForm/index.js
@@ -1,8 +1,30 @@
-import React from 'react'
-import { Form, FormTitle, LoginDescriptionUl, ImageCompaniesWrapper, ImageCompanies, LoginDescriptionUlImage, LoginDescriptionLi, LoginDescriptionWrapper, LoginDescriptionTitle, FormWrapper, FormtextWrapper, FormInputWrapper, FormInput, FormButton, Formtext } from './style'
-import imgCheckList from "../../assets/check.png"
+import React from 'react';
+import { Form, FormTitle, LoginDescriptionUl, ImageCompaniesWrapper, ImageCompanies, LoginDescriptionUlImage, LoginDescriptionLi, LoginDescriptionWrapper, LoginDescriptionTitle, FormWrapper, FormtextWrapper, FormInputWrapper, FormInput, FormButton, Formtext } from './style';
+import imgCheckList from "../../assets/check.png";
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+
+const schema = z.object({
+  email: z.string().email('Use a valid email: myemail@gmail.com.'),
+  password: z.string().min(6, 'Password must be at least 6 characters long.'),
+});
+
 
 export default function LoginForm() {
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: zodResolver(schema),
+    });
+
+    const onSubmit = (data) => {
+        console.log('Dados válidos:', data);
+    };
+
   return (
     <>
     <FormWrapper>
@@ -15,25 +37,27 @@ export default function LoginForm() {
             </LoginDescriptionUl>
             
         </LoginDescriptionWrapper>
-        <Form>
+        <Form onSubmit={handleSubmit(onSubmit)}>
             <FormTitle>Already registered?</FormTitle>
             <FormInputWrapper>
-                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                     <path fill="none" d="M0 0h24v24H0z"></path>
                     <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"></path>
                 </svg>
-                <FormInput type='email' placeholder='E-mail' name='email'/>
+                <FormInput {...register('email')} type='email' placeholder='E-mail' name='email'/>
             </FormInputWrapper>
+            {errors.email && <p style={{ color: 'red' }}>{errors.email.message}</p>}
             <FormInputWrapper>
-                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                     <g>
                         <path fill="none" d="M0 0h24v24H0z"></path>
                         <path d="M18 8h2a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1h2V7a6 6 0 1 1 12 0v1zm-2 0V7a4 4 0 1 0-8 0v1h8zm-5 6v2h2v-2h-2zm-4 0v2h2v-2H7zm8 0v2h2v-2h-2z"></path>
                     </g>
                 </svg>
-                <FormInput type='password' placeholder='Password' name='password'/>
+                <FormInput {...register('password')}  type='password' placeholder='Password' name='password'/>
             </FormInputWrapper>
-            <FormButton onClick={(e) => e.preventDefault()}>Login</FormButton>
+            {errors.password && <p style={{ color: 'red' }}>{errors.password.message}</p>}
+            <FormButton>Login</FormButton>
 
             <FormtextWrapper>
                 <Formtext>Forgot my password</Formtext>
